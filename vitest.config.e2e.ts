ba3536efbc6bc51a1d839c9c42a503b0267f0584
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-26 20:11:33 -0300

    Creating isolated database for e2e testing

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'], // Sera chamado antes de cada teste
   },
   plugins: [
     tsConfigPaths(),
