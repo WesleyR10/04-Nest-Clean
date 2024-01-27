020308058a0b0011ab6c1ec2610785bba5418e2e
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-27 20:10:29 -0300

    Implementing question repository layer infra

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question { // Método estático para nao precisar instanciar a classe
@@ -17,5 +17,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id), // Como passei o Id, o método create vai entender que é uma entidade existente
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput { // Utilizou o QuestionUncheckedCreateInput para não precisar passar o ID
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
