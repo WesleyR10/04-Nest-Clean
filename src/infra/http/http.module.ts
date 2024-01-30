742b9c764d76271010241851d82fb30233ada3da
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-30 17:44:44 -0300

    Creating controller Get Question by slug

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -10,6 +10,8 @@ import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-case
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -18,7 +20,8 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
   ],
-  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase, RegisterStudentUseCase,AuthenticateStudentUseCase,],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase, RegisterStudentUseCase, AuthenticateStudentUseCase, GetQuestionBySlugUseCase,],
 })
 export class HttpModule {}
