b6d0e17f702164d2a5f2b81daa34b5276e72b3e2
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-30 18:18:52 -0300

    Using factories in e2e tests

--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -5,6 +5,9 @@ import {
   Student,
   StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
 
 export function makeStudent(
   override: Partial<StudentProps> = {},
@@ -21,4 +24,19 @@ export function makeStudent(
   )
 
   return student
+}
+
+@Injectable()
+export class StudentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data) // Utilizando a função makeStudent acima para criar um novo estudante
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student), // Utilizando o mapper para converter a entidade para o formato do Prisma
+    })
+
+    return student
+  }
 }
