d6c0d740167af08e18fb1d81c8491d4c36b037d5
WesleyR10 <wesleyribas2015@gmail.com>
2024-02-02 00:52:29 -0300

    Edit question attachments - controller useCase and test e2e

--- src/infra/http/controllers/edit-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -2,7 +2,9 @@ import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 import { QuestionFactory } from 'test/factories/make-question'
+import { QuestionAttachmentFactory } from 'test/factories/make-question-attachments'
 import { StudentFactory } from 'test/factories/make-student'
 
 import { AppModule } from '@/infra/app.module'
@@ -14,12 +16,19 @@ describe('Edit question (E2E)', () => {
   let prisma: PrismaService
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
+  let attachmentFactory: AttachmentFactory
+  let questionAttachmentFactory: QuestionAttachmentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile()
 
     app = moduleRef.createNestApplication()
@@ -27,6 +36,8 @@ describe('Edit question (E2E)', () => {
     prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
@@ -37,9 +48,24 @@ describe('Edit question (E2E)', () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
     })
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment1.id,
+      questionId: question.id,
+    })
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment2.id,
+      questionId: question.id,
+    })
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment()
 
     const questionId = question.id.toString()
 
@@ -49,6 +75,7 @@ describe('Edit question (E2E)', () => {
       .send({
         title: 'New title',
         content: 'New content',
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
       })
 
     expect(response.statusCode).toBe(204)
@@ -61,5 +88,23 @@ describe('Edit question (E2E)', () => {
     })
 
     expect(questionOnDatabase).toBeTruthy()
+
+  const attachmentsOnDatabase = await prisma.attachment.findMany({
+    where: {
+      questionId: questionOnDatabase?.id,
+    },
+  })
+
+  expect(attachmentsOnDatabase).toHaveLength(2)
+  expect(attachmentsOnDatabase).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: attachment1.id.toString(),
+      }),
+      expect.objectContaining({
+        id: attachment3.id.toString(),
+        }),
+      ]),
+    )
   })
 })
--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -16,6 +16,7 @@ import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema)
@@ -33,14 +34,14 @@ export class EditQuestionController {
     @CurrentUser() user: UserPayload,
     @Param('id') questionId: string,
   ) {
-    const { title, content } = body
+    const { title, content, attachments } = body
     const userId = user.sub
 
     const result = await this.editQuestion.execute({
       title,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
       questionId,
     })
 
