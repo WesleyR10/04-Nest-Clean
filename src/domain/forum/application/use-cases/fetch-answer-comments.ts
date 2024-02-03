887f3c182fb596190fb9ef848928bae2a29951fc
WesleyR10 <wesleyribas2015@gmail.com>
2024-02-03 11:34:44 -0300

    Answer comments with author - domain layer and infra - test unit and test e2e

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common'
 
 import { Either, right } from '@/core/either'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
-import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface FetchAnswerCommentsUseCaseRequest {
   answerId: string
@@ -12,7 +13,7 @@ interface FetchAnswerCommentsUseCaseRequest {
 type FetchAnswerCommentsUseCaseResponse = Either<
   null,
   {
-    answerComments: AnswerComment[]
+    comments: CommentWithAuthor[]
   }
 >
 
@@ -24,13 +25,16 @@ export class FetchAnswerCommentsUseCase {
     answerId,
     page,
   }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
-    const answerComments =
-      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+    const comments =
+    await this.answerCommentsRepository.findManyByAnswerIdWithAuthor(
+      answerId,
+      {
         page,
-      })
+      },
+    )
 
     return right({
-      answerComments,
+      comments,
     })
   }
 }
